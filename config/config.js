cdcc6cb8932552d42232c030ced1096505e924fb
onedayonecommit <rudghks0981@gmail.com>
2022-09-13 09:51:28 +0900

    db change

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   dev: {
     username: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     database: process.env.DB_DATABASE_NAME,
     dialect: "mysql",
   },
